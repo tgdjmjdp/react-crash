85d81d252a2d72c70705a21863c4220e3d674ff8
tgdjmjdp <tgdjmjdp2@gmail.com>
2019-08-17 11:02:21 +0700

    connect b with mongoClient

--- config/db.js
+++ config/db.js
@@ -1,14 +1,44 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
+const db = 'mongodb://127.0.0.1:27017';
+const MongoClient = require('mongodb').MongoClient, format = require('util').format; 
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(db);
-        console.log('MongoDB Connected');
-
+        await MongoClient.connect(db, {});
+        console.log('DB connected');
     } catch (err) {
         console.error(err.message);
         process.exit(1);
+
     }
 }
+
+module.exports = connectDB;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const MongoClient = require('mongodb').MongoClient, format = require('util').format;
+
+MongoClient.connect('mongodb://127.0.0.1:27017', (err, db) => {
+    if(err){
+        throw err;
+    }else{
+        console.log("connected");
+
+    }
+    db.close();
+})
+
+ */
