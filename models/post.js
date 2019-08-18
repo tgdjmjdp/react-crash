629d4ac9ad449465bcea53ec7ccff92ac85ebb0c
tgdjmjdp <tgdjmjdp2@gmail.com>
2019-08-18 19:25:05 +0700

    insert post

--- models/post.js
+++ models/post.js
@@ -23,7 +23,7 @@ const PostSchema = new mongoose.Schema({
     likes: [
         {
             user: {
-                type: Schema.Types.ObjectId,
+                type: mongoose.Schema.Types.ObjectId,
                 required: true,
                 ref: 'users'
             },
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+const User = require('../../models/user');
+const Profile = require('../../models/profile');
+const Post = require('../../models/post');
 
-router.get('/', (req, res) => res.send('User posts'));
+router.post('/', [auth, [
+    check('text', 'text is required').not().isEmpty()
+]], async (req, res) => {
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        });
+
+        const post = await newPost.save();
+
+        if (post) {
+            console.log('====================================');
+            console.log("post created");
+            console.log('====================================');            
+        }
+
+        res.json(post);
+
+    } catch (error) {
+
+        console.log('====================================');
+        console.error(error.message);
+        console.log('====================================');
+        res.status(500).send('Server Error');
+
+    }
+});
 
 module.exports = router;
