8e764c2c06803828d885358418508ca38fbd92d7
tgdjmjdp <tgdjmjdp2@gmail.com>
2019-08-18 07:03:36 +0700

    create profile model

--- models/profile.js
+++ models/profile.js
@@ -111,4 +111,4 @@ const ProfileSchema = new mongoose.Schema({
     }
 });
 
-module.exports = Profile = mongoose.model('users', ProfileSchema);
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
